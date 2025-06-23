eb1b734dac7c63f9ceafcfd67147b2589d00c632
OtavioAM <otavio.m@estudante.ifmt.edu.br>
2025-06-23 16:39:14 -0400

    Update FileDemonstration.java

--- FileDemonstration.java
+++ FileDemonstration.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 
-/**
- *
- * @author Aluno
- */
 import java.io.File;
 import java.util.Scanner;
 
